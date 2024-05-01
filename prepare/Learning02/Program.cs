4b953e7115613c11a3306e4eee1a8bbe5bd2a805
Ricardo Salazar <rickylokx@gmail.com>
2024-05-01 22:22:58 +0200

    w2 abstraccion assigment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        job1._startYear = 2001;
+        job1._endYear = 2005;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Microsoft";
+        job2._startYear = 2005;
+        job2._endYear = 2010;
+
+        Resume resume1 = new Resume();
+        resume1._names = "Ricardo Salazar";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
+
+        
+        
     }
 }
